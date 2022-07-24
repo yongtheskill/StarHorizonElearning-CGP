3fdf767a2b904f2197b716b66360bab2d2b7fdba
yongtheskill <yongee2003@gmail.com>
2022-07-24 19:19:28 +0800

    Update admin.py

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -14,7 +14,7 @@ class QuizAdmin(admin.ModelAdmin):
     list_display.append("download_link")
 
     fields = ('quizName', 'quizDueDate', 'passingScore', 'module')
-    readonly_fields = ('quizID')
+    readonly_fields = ('quizID',)
 
     def download_link(self, obj):
         return format_html("<a href='/quizzes/dl/?id={id}'>Download</a>", id=obj.quizID)
@@ -24,7 +24,7 @@ class QuizAdmin(admin.ModelAdmin):
 
 
 class QuestionAttemptInline(admin.TabularInline):
-    fields = ('question', 'type', 'isCorrect')
+    fields = ('question', 'type', 'isCorrect',)
     model = QuestionAttempt
     readonly_fields = ('question', 'type')
     exclude = ('saAnswer', 'mcAnswer', 'cbAnswer')
