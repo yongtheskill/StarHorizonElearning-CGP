260a9e4b73e5b73751056ee95687e7d0bee60a99
yongtheskill <yongee2003@gmail.com>
2023-08-12 00:37:22 +0800

    Update wsgi.py

--- StarHorizonElearning/wsgi.py
+++ StarHorizonElearning/wsgi.py
@@ -20,7 +20,7 @@ from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root="/static")
+application = WhiteNoise(application, root="static", prefix="static/")
 
 
 """
